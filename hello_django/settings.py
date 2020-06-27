16388e0be682bdf6febc84f621453f7b4f1996a7
Elany Branches <elany7@gmail.com>
2020-06-27 09:26:01 -0300

    Update settings.py

--- hello_django/settings.py
+++ hello_django/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@#gbg=sz^vb)v7_q(h*$=sc$ebyk!axd1x&d1qz&*#xgwo8-4s'
+SECRET_KEY = ***************************************************
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
